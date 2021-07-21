60d48b485ef658de1628b53e420a974d21e7c76b
AnjaliSreekumar668 <87770148+AnjaliSreekumar668@users.noreply.github.com>
2021-07-21 21:34:04 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,20 @@
+package com.ust.examples;
+
+public class example1 {
+String name;
+double num;
+static int count;
+
+public static void main(String args[])
+{
+	int age =10;
+	System.out.println(age);
+	example1 e = new example1();
+	e.name="alona";
+	e.num=4.0;
+	count=8;
+	System.out.println(e.name);
+	System.out.println(e.num);
+	System.out.println(count);
+}
+}
