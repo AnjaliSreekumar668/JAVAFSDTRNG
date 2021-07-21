244e5d39be4d1fbd3179202eef444f45281a7067
AnjaliSreekumar668 <87770148+AnjaliSreekumar668@users.noreply.github.com>
2021-07-21 22:45:07 +0530

    Add files via upload

---
+++ Wrapper/wrapper2.java
@@ -0,0 +1,16 @@
+package com.ust.examples;
+
+public class wrapper2 {
+
+	public static void main(String[] args) {
+		Integer myInt = 100;
+		String myString = myInt.toString();
+		
+		
+		    
+		    System.out.println(myString.length());
+		   
+
+	}
+
+}
