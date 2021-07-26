0d314a4adfbd554bfdfe8e20df22b6230a4d7e35
AnjaliSreekumar668 <87770148+AnjaliSreekumar668@users.noreply.github.com>
2021-07-26 19:41:16 +0530

    Add files via upload

---
+++ React/FormattedDat.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+
+function FormattedDate(props) {
+    return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+  }
+  
+  class FormattedDat extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <FormattedDate date={this.state.date} />
+        </div>
+      );
+    }
+  }
+  
+  function App() {
+    return (
+      <div>
+        <FormattedDat />
+        <FormattedDat />
+        <FormattedDat />
+      </div>
+    );
+  }
+  
+  export default FormattedDat;
+  
