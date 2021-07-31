26cf0bafe0ca1efe62666504d852a862864fd809
AnjaliSreekumar668 <87770148+AnjaliSreekumar668@users.noreply.github.com>
2021-07-31 23:25:02 +0530

    Add files via upload

---
+++ Servelet/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.ust.examples;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(
+    name="WelcomeServlet",
+    description="Servlet with Annotation",
+    urlPatterns={"/WelcomeServlet","/HelloWorld"}
+    )
+public class HelloWorld extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        System.out.println("Get method of Welcome Servlet");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+    {
+        System.out.println("Post method of Welcome Servlet");
+    }
+}
+    
