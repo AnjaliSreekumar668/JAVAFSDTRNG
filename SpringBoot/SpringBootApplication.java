43b2b1890a30917549ff8ed7d2aa0ddbd32b73d9
AnjaliSreekumar668 <87770148+AnjaliSreekumar668@users.noreply.github.com>
2021-08-15 12:49:53 +0530

    Add files via upload

---
+++ SpringBoot/SpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.ust.example;
+
+import org.springframework.boot.SpringApplication;
+
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+
+public class SpringBootApplication {
+
+	public static void main(String array [])
+	{
+		SpringApplication.run(SpringApplication.class, array);
+	}
+}
+	
