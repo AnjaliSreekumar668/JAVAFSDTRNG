d610d534e0009ca8d9f58f9650e09a66dadadc90
AnjaliSreekumar668 <87770148+AnjaliSreekumar668@users.noreply.github.com>
2021-08-16 21:58:05 +0530

    Add files via upload

---
+++ Spring-boot-crud/SpringBootBookAppln.java
@@ -0,0 +1,14 @@
+package com.ust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookAppln {
+
+	public static void main(String[] args)   
+	{  
+	SpringApplication.run(SpringBootBookAppln.class, args);  
+	}  
+	
+}
