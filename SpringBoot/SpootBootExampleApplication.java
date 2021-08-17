b8d96be4ccebd3e187319af802b814ddd3225268
AnjaliSreekumar668 <87770148+AnjaliSreekumar668@users.noreply.github.com>
2021-08-17 16:18:53 +0530

    Add files via upload

---
+++ SpringBoot/SpootBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javapoint.spootbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpootBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpootBootExampleApplication.class, args);
+	}
+
+}
